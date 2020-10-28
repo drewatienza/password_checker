1b1cc68e523b6ba06f8312ce8e68b5e90fe45ea9
drewatienza <atienza.drew@gmail.com>
2020-10-27 17:39:38 -0700

    Finished project.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Password Checker
+
+This is a password checker program written in Python.
+
+The program is run on the command line and multiple passwords can be checked at once.
+
+On the command line, simple go to the folder where this file is and run:
+
+> python3 checkmypass.py password1 password2
+
+Simply type in the password that you want to check after typing checkmypass.py.
+
+To check multiple passwords, simply type in multipe passwords separated by a space.
--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -10,9 +11,30 @@ def request_api_data(query_char):
     return res
 
 
+def get_password_leaks_count(hashes, hash_to_checks):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_checks:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
 
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times.  You should probably change your password.')
+        else:
+            print(f'Congratulations!  {password} was NOT found!')
+    return 'Done!'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
